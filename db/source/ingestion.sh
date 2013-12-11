8cff3cc74340471976ddf9aa25a229572725dd77
matt <matthewlkiefer@gmail.com>
2013-12-11 15:53:52 -0600

    mod ingestion sh script

--- db/source/ingestion.sh
+++ db/source/ingestion.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-python ingestion.py;
-python ../../manage.py loaddata output_test.json
+PYTHONPATH=$PYTHONPATH:/home/user/django_projects/cooktax/
+export DJANGO_SETTINGS_MODULE=cooktax.settings
+python ingestion.py
+#python ../../manage.py loaddata output_test.json
