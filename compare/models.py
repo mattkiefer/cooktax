7dd594296808340ad06a4175546a5966639c10fb
matt <matthewlkiefer@gmail.com>
2014-06-21 20:05:00 -0600

    ingestion rework with fk corrections

--- compare/models.py
+++ compare/models.py
@@ -10,8 +10,8 @@ class Property(models.Model):
     pin = models.IntegerField(primary_key=True,db_index=True) # index
     ml_address=models.CharField(max_length=50)
     pl_address=models.CharField(max_length=50)
-    town_id=models.ForeignKey('Town')
-    cls_id=models.ForeignKey('BldgClass')
+    town=models.ForeignKey('Town')
+    bldg_cls=models.ForeignKey('BldgClass')
     triennial=models.CharField(max_length=4) # n/w city s/w
     land_sqft=models.IntegerField() # good story idea here
     bldg_sqft=models.IntegerField()
@@ -25,16 +25,16 @@ class Property(models.Model):
     pri_bldg=models.IntegerField()
     pri_total=models.IntegerField()
     pri_mktval=models.IntegerField()
-    res_id=models.ForeignKey('ResType')
-    bldg_id=models.ForeignKey('BldgUse')
+    res_type=models.ForeignKey('ResType')
+    bldg_use=models.ForeignKey('BldgUse')
     no_of_apts=models.IntegerField()
-    ext_const_id=models.ForeignKey('ExtConst')
+    ext_const=models.ForeignKey('ExtConst')
     full_bath=models.IntegerField()
     half_bath=models.IntegerField()
-    bsmt_id=models.ForeignKey('Bsmt')
-    attic_id=models.ForeignKey('Attic')
+    bsmt=models.ForeignKey('Bsmt')
+    attic=models.ForeignKey('Attic')
     fire_pl=models.IntegerField()
-    gar_id=models.ForeignKey('Garage')
+    gar=models.ForeignKey('Garage')
     age=models.IntegerField()
     pass_no=models.IntegerField() # 1, 2
     year=models.IntegerField() # 2013
@@ -43,35 +43,36 @@ class Property(models.Model):
         return unicode(self.pin)
 
 class Town (models.Model):
-    town_id = models.IntegerField(primary_key=True,db_index=True)
-    town = models.CharField(max_length=20, unique=True)
+    town = models.IntegerField(primary_key=True,db_index=True)
+    town_desc = models.CharField(max_length=20, unique=True)
 
 class BldgClass (models.Model):
-    cls_id=models.IntegerField(primary_key=True,db_index=True) # primary key
-    clsdescr=models.CharField(max_length=20)#, unique=True) # cls *** check this
+    bldg_cls=models.IntegerField(primary_key=True,db_index=True) # primary key
+    bldg_cls_desc=models.CharField(max_length=20)#, unique=True) # cls *** check this
+    old_cls=models.CharField(max_length=4)
 
 class ResType (models.Model):
-    res_id = models.IntegerField(primary_key=True,db_index=True) 
-    res_type = models.CharField(max_length=11, unique=True)
+    res_type = models.IntegerField(primary_key=True,db_index=True) 
+    res_type_desc = models.CharField(max_length=11, unique=True)
 
 class BldgUse (models.Model):
-    bldg_id = models.IntegerField(primary_key=True,db_index=True) 
-    bldg_use = models.CharField(max_length=13, unique=True)
+    bldg_use = models.IntegerField(primary_key=True,db_index=True) 
+    bldg_use_desc = models.CharField(max_length=13, unique=True)
 
 class ExtConst (models.Model):
-    ext_const_id = models.IntegerField(primary_key=True,db_index=True)
-    ext_const = models.CharField(max_length=13, unique=True) 
+    ext_const = models.IntegerField(primary_key=True,db_index=True)
+    ext_const_desc = models.CharField(max_length=13, unique=True) 
 
 class Bsmt (models.Model):
-    bsmt_id = models.IntegerField(primary_key=True,db_index=True)
-    bsmt =  models.CharField(max_length=26, unique=True) 
+    bsmt = models.IntegerField(primary_key=True,db_index=True)
+    bsmt_desc =  models.CharField(max_length=26, unique=True) 
 
 class Attic (models.Model):
-    attic_id = models.IntegerField(primary_key=True,db_index=True)
-    attic = models.CharField(max_length=21, unique=True )
+    attic = models.IntegerField(primary_key=True,db_index=True)
+    attic_desc = models.CharField(max_length=21, unique=True )
 
 class Garage (models.Model):
-    gar_id = models.IntegerField(primary_key=True,db_index=True) 
-    gar_des = models.CharField(max_length=20, unique=True)
+    gar = models.IntegerField(primary_key=True,db_index=True) 
+    gar_desc = models.CharField(max_length=20, unique=True)
 
 
--- db/source/ingestion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PYTHONPATH=$PYTHONPATH:/home/user/django_projects/cooktax/
-export DJANGO_SETTINGS_MODULE=cooktax.settings
-python ingestion.py
-#python ../../manage.py loaddata output_test.json
